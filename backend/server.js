746edfe82d982c74d0c80242c8b52e2714a58bf8
zljokerzz <thomadavid9@gmail.com>
2025-09-25 22:08:45 +0200

    first commit

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("Server is up and running!");
+});
+
+app.listen(PORT, () => {
+  console.log("Server is running on port", PORT);
+});
