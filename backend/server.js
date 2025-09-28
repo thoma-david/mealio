c160855fe25817431eefd84133d834a0ca5e1834
zljokerzz <thomadavid9@gmail.com>
2025-09-28 20:13:48 +0200

    finished backend prototype, added Login page, hooks, public, protected routes to the frontend

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,33 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
-import User from "./models/user.model.js";
 import authRouter from "./routes/authRouter.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 
 app.use(express.json()); //middleware
+app.use(cookieParser());
+
+
+
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true // falls du Cookies oder Auth-Headers brauchst
+}));
 
 const PORT = process.env.PORT || 5000;
 
 //API Endpoints
 app.use("/api/auth", authRouter);
 
-app.listen(PORT, () => {
-  connectDB();
-  console.log("Server is running on port", PORT);
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}).catch((error) => {
+  console.error("DB connection failed:", error);
 });
